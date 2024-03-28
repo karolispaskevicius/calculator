4bf11af0da46f0ada3e28e089086772a0d287c2f
Karolis Paskevicius <kp704595@gmail.com>
2024-03-28 16:43:31 +0200

    Misstype on line 23

--- src/main/java/com/spring/calculator/CalculatorApplication.java
+++ src/main/java/com/spring/calculator/CalculatorApplication.java
@@ -20,6 +20,6 @@ public class CalculatorApplication {
 	}
 	@GetMapping
 	public String hello(@RequestParam(value= "name", defaultValue= "World") String name){
-		return String.format("Hello $s!", name);
+		return String.format("Hello %s!", name);
 	}
 }
